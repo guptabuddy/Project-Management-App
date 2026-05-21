45119b809905f13245ad884ed02f4c346f18d3e1
guptabuddy <guptabudy@gmail.com>
2026-05-21 18:14:29 +0530

    First Commit, add Project Sidebar

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import ProjectSidebar from "./components/ProjectSidebar.jsx";
+
+export default function App() {
+	return (
+		<main className="h-screen my-8">
+			<ProjectSidebar />
+		</main>
+	);
+}
