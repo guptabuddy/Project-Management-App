af99aea532f2a419ea699833421df6f18d88595d
guptabuddy <guptabudy@gmail.com>
2026-05-23 18:00:15 +0530

    Make Create Projects button in ProjectSidebar & NoProjectSelected component work, plus Add State, so that the newProject component work.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,45 @@
-// import NewProject from "./components/NewProject.jsx";
+import { useState } from "react";
 import NoProjectSelected from "./components/NoProjectSelected.jsx";
 import ProjectSidebar from "./components/ProjectSidebar.jsx";
+import NewProject from "./components/NewProject.jsx";
 
 export default function App() {
+	const [projectsState, setProjectsState] = useState({ selectedProjectID: undefined, projects: [] });
+
+	function handleStartAddProject() {
+		setProjectsState((prevProjects) => ({
+			...prevProjects,
+			selectedProjectID: null,
+		}));
+	}
+
+	function handleAddProject(projectData) {
+		setProjectsState((prevState) => {
+			const newProject = {
+				...projectData,
+				id: Math.random(),
+			};
+
+			return {
+				...prevState,
+				projects: [...prevState.projects, newProject],
+			};
+		});
+	}
+
+	console.log(projectsState);
+
+	let content;
+	if (projectsState.selectedProjectID === null) {
+		content = <NewProject onAdd={handleAddProject} />;
+	} else if (projectsState.selectedProjectID === undefined) {
+		content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
+	}
+
 	return (
 		<main className="h-screen my-8 flex gap-8">
-			<ProjectSidebar />
-			{/* <NewProject /> */}
-			<NoProjectSelected />
+			<ProjectSidebar onStartAddProject={handleStartAddProject} />
+			{content}
 		</main>
 	);
 }
--- src/components/ProjectSidebar.jsx
+++ src/components/ProjectSidebar.jsx
@@ -1,11 +1,11 @@
 import Button from "./Button.jsx";
 
-export default function ProjectSidebar() {
+export default function ProjectSidebar({ onStartAddProject }) {
 	return (
 		<aside className="w-1/3 px-8 py-16 bg-stone-900 text-white md:w-72 rounded-r-xl">
 			<h2 className="mb-8 font-bold uppercase md:text-xl text-stone-200">Your Projects</h2>
 			<div>
-				<Button>+Add Projects</Button>
+				<Button onClick={onStartAddProject}>+Add Projects</Button>
 			</div>
 			<ul></ul>
 		</aside>
